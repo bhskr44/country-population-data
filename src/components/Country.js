fa92d687b472c50078c52a644469fc5c96e1d775
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2023-01-07 17:28:09 +0530

    Create Country.js

---
+++ src/components/Country.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Country = ({ data }) => {
+  const navigate = useNavigate();
+
+  const handleRoute = () => {
+    navigate(`/details/${data.name}`, { state: data });
+  };
+
+  return (
+    <button type="button" onClick={handleRoute} className="country">
+      <img className="country-img" src={data.flag} alt="Country Flag" />
+      <div className="details">
+        <h2>{data.name}</h2>
+        <p>
+          Population:
+          <span>{data.population}</span>
+        </p>
+        <p>
+          Continent:
+          <span>{data.region}</span>
+        </p>
+        <p>
+          Capital:
+          <span>{data.capital}</span>
+        </p>
+        {/* <p>.</p>
+      <p>.</p>
+      <p>click for more details</p> */}
+      </div>
+    </button>
+  );
+};
+
+export default Country;

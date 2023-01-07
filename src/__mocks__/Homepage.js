a3b55b2e90e6ff03aecd7d0aeb415d51a5308409
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2023-01-07 22:50:13 +0530

    Added Test

---
+++ src/__mocks__/Homepage.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import CountriesList from '../components/ContriesList';
+import {BrowserRouter} from 'react-router-dom';
+
+
+const Homepage = ({ countries }) => {
+  const [searchItem, setSearchItem] = useState('');
+
+  const newCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(searchItem.toLowerCase()) ||
+      country.region.toLowerCase().includes(searchItem.toLowerCase())
+  );
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchItem(e.target.value);
+  };
+
+  return (
+    <div>
+      <header className="home-header">
+        <div className="logo">
+          <TravelExploreIcon className="svg_icons" />
+        </div>
+        <div className="search">
+          {' '}
+          <input
+            type="text"
+            name="searchItem"
+            placeholder="search for a country"
+            value={searchItem}
+            onChange={handleSearch}
+          />
+        </div>
+      </header>
+
+      {searchItem.length ? (
+        <CountriesList countries={newCountries} />
+      ) : (
+        <BrowserRouter>
+          <CountriesList countries={countries} />
+        </BrowserRouter>
+      )}
+    </div>
+  );
+};
+
+export default Homepage;

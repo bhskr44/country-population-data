0d77d0b223ef36651fce970d204c0cd026f2933d
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2023-01-07 17:28:36 +0530

    Create Homepage.js

---
+++ src/components/Homepage.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getCountries from '../redux/api';
+import CountriesList from './ContriesList';
+
+const Homepage = () => {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries.countries);
+
+  const [searchItem, setSearchItem] = useState('');
+
+  console.log(countries);
+
+  useEffect(() => {
+    if (countries.length === 0) {
+      dispatch(getCountries());
+    }
+  }, [dispatch, countries.length]);
+
+  const newCountries = countries.filter(
+    (country) => country.name.toLowerCase().includes(searchItem.toLowerCase())
+      || country.region.toLowerCase().includes(searchItem.toLowerCase()),
+  );
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchItem(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Where in the World</h1>
+      <input
+        type="text"
+        name="searchItem"
+        placeholder="search for a country"
+        value={searchItem}
+        onChange={handleSearch}
+      />
+      {searchItem.length ? (
+        <CountriesList countries={newCountries} />
+      ) : (
+        <CountriesList countries={countries} />
+      )}
+    </div>
+  );
+};
+
+export default Homepage;

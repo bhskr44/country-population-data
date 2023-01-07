a130d9d7d0648111e50c678366852f0062ab4601
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2023-01-07 17:28:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import HomePage from './components/home/HomePage';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="main-container">
+      <div className="container">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/details/:id" element={<DetailsPage />} />
         </Routes>
       </div>
     </BrowserRouter>
